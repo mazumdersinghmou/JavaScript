e384d419226ae6714dbf75ed4b0364795a6440e5
Mou Mazumder Singh <mazumdersinghmou@gmail.com>
2025-08-01 17:33:14 +0530

    reverse an array

---
+++ reverseArray.js
@@ -0,0 +1,13 @@
+// Reverse an Array
+// Input: [1, 2, 3]
+// Output: [3, 2, 1]
+
+function reverseArray(arr){
+    let reversed = []
+    for(let i = arr.length -1; i >= 0; i--){
+         reversed.push(arr[i]);
+    }
+    console.log(reversed);
+}
+
+reverseArray([1,2,3,4,5])
