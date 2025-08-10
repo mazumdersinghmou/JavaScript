37ab9691e6dd177fc13a512f794e4aeb0ed89233
Mou Mazumder Singh <mazumdersinghmou@gmail.com>
2025-08-11 00:32:01 +0530

    program to shuffle the deck of cards

---
+++ shuffleDeckOfCards.js
@@ -0,0 +1,44 @@
+// program to shuffle the deck of cards
+
+// declare card elements
+
+const suits = ["Spades", "Diamond", "Heart", "Club"];
+const values = [
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+];
+
+let deck = [];
+
+// create a deck of cards
+for (let i = 0; i < suits.length; i++) {
+  for (let x = 0; x < values.length; x++) {
+    let card = { Value: values[x], Suit: suits[i] };
+    deck.push(card);
+  }
+}
+
+// shuffle the cards
+for (let i = deck.length - 1; i > 0; i--) {
+  let j = Math.floor(Math.random() * i);
+  let temp = deck[i];
+  deck[i] = deck[j];
+  deck[j] = temp;
+}
+
+console.log("The first 5 cards are: ");
+
+for(i = 0; i < 5; i++){
+    console.log(`${deck[i].Suit} of ${deck[i].Value}`);
+}
