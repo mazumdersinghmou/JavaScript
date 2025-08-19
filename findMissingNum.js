1b5f7d9713bab295304f86664206ff5e48770cf2
Mou Mazumder Singh <mazumdersinghmou@gmail.com>
2025-08-19 19:54:13 +0530

    Add files via upload

---
+++ findMissingNum.js
@@ -0,0 +1,9 @@
+// Find Missing Number
+
+function missingNumber(arr, n) {
+  let total = (n * (n + 1)) / 2;
+  let sum = arr.reduce((a, b) => a + b, 0);
+  return total - sum;
+}
+
+console.log(missingNumber([1, 2, 3, 5, 6], 6));
